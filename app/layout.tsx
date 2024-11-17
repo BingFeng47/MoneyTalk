02c703bc3905758cb58ce8acb69fa8497c4b85e5
Bing <145952666+This-Is-Bing@users.noreply.github.com>
2024-11-17 12:39:03 +0800

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import "./globals.css";
 import { ThemeProvider } from "next-themes";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
-import { Bot } from "lucide-react";
+import { Bird, Bot } from "lucide-react";
 import GoogleAnalyticsProvider from "@/components/ui/GoogleAnalyticsProvider";
 import { ThemeButton } from "@/components/ui/ThemeButton";
 
@@ -37,9 +37,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <GoogleAnalyticsProvider/>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+      
       <ThemeProvider
           attribute="class"
           defaultTheme="system"
@@ -47,43 +46,45 @@ export default function RootLayout({
           disableTransitionOnChange
       >
         {/* Header */}
-        <header className="px-4 lg:px-6 h-20 flex items-center sticky top-0 z-50 bg-background pt-1">
+        <header className="px-4 lg:px-6 h-20 flex items-center sticky top-0 z-50 bg-secondary pt-1">
         
           {/* Logo */}
-          <a className="flex items-center justify-center" href="/">
-            <Bot className="h-8 w-8" />
-          </a>
+            <a className="flex items-center justify-center" href="/">
+            <Bird className="h-8 w-8 text-[#682bd7] dark:text-white" />
+            <h1 className="text-xl px-2 text-primary font-extrabold ">Money<span className="font-bold text-xl text-secondary-foreground">Talk</span></h1>
+            </a>
+            
           
           {/* Nav Items */}
-          <nav className="ml-auto flex gap-2 lg:gap-4 items-center">
+            <nav className="ml-auto  gap-2 lg:gap-4 items-center hidden sm:flex">
             
-            
-            <Link href={"/chatbot"}>
-              <Button variant= {"outline"} className="lg:text-sm py-5">
-                AI Team Builder
-              </Button>
-            </Link>
-
-            <Link href={"/players"}>
-              <Button variant= {"outline"} className="lg:text-sm py-5">
-                Players
+            <Link href={"/"}>
+              <Button variant={"outline"} className="lg:text-sm py-5">
+              Dashboard
               </Button>
             </Link>
 
             <Link href={"/about"}>
-              <Button variant= {"outline"} className="lg:text-sm py-5">
-                About Us
+              <Button variant={"outline"} className="lg:text-sm py-5">
+              About Us
               </Button>
             </Link>
 
             <Link href={"/docs"}>
-              <Button variant= {"outline"} className="lg:text-sm py-5">
-                Docs
+              <Button variant={"outline"} className="lg:text-sm py-5">
+              Documentation
+              </Button>
+            </Link>
+            
+            <Link href={"/players"}>
+              <Button variant={"default"} className="lg:text-sm py-5 font-bold">
+              Try it Out
               </Button>
             </Link>
 
-            <ThemeButton/>
-          </nav>
+            <ThemeButton />
+
+            </nav>
 
         </header>
 
